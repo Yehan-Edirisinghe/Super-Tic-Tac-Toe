509b7f66e07a863a22bbc30fa4084556cba246be
Yedi278 <yehan278@gmail.com>
2023-10-19 18:12:32 +0200

    check tris func

--- game.py
+++ game.py
@@ -9,6 +9,7 @@ black   =   0,  0,  0
 class Board:
 
     grid    = np.empty((9,9))
+
     bigTris = np.empty((3,3))
 
     def __init__(self,size):
@@ -35,8 +36,29 @@ def checkTris(x,y,board):
 
     for i in range(3*x,3*x+3):
         for j in range(3*y,3*y+3):
-            if board.grid[i,j] == 1:
+
+            center = (i-3*x ,j-3*y)
+
+            if (i-3*x)==(j-3*y) and board.grid[i,j] == 1:
+
                 
+                
+                if board.grid[i+1,j] == board.grid[i-1,j] == 1:
+                    return True
+                if board.grid[i,j+1] == board.grid[i,j-1] == 1:
+                    return True
+                if board.grid[i+1,j+1] == board.grid[i-1,j-1] == 1:
+                    return True
+                if board.grid[i+1,j-1] == board.grid[i-1,j+1] == 1:
+                    return True
+                
+            elif board.grid[center[0]-1,j] == 1:
+
+                if board.grid[center[0]-1,j+1] == board.grid[center[0]-1,j-1] == 1:
+                    return True
+
+
+
 
 def checkBigTris():
     for i in range(4):
@@ -57,7 +79,11 @@ def play(canvas,board:Board,turn):
         marker = 2
 
     board.grid[move[0],move[1]] = marker
-    checkTris(0,1,board)
+
+    for i in range(3):
+        for j in range(3):
+            if checkTris(i,j,board):
+                print("Tris!")
 
 
     return True
