ba646e4253ed908b6d43c9b623c78541d0ad6c40
Yedi278 <yehan278@gmail.com>
2023-10-19 13:06:12 +0200

    separation into library

---
+++ game.py
@@ -0,0 +1,41 @@
+import pygame
+import numpy as np
+
+white   = 255,255,255
+red     = 255,  0,  0
+black   =   0,  0,  0
+
+    
+def draw_X(pos,canvas):
+    size = canvas.get_size()
+    k = size[0]/20
+    l = size[1]/20
+    i = pos[0]-k,pos[1]-k
+    f = pos[0]+l,pos[1]+l
+    
+    i1 = pos[0]-k,pos[1]+k
+    f1 = pos[0]+k,pos[1]-k
+
+    pygame.draw.line(canvas,white,i,f)
+    pygame.draw.line(canvas,white,i1,f1)
+
+def draw_O(pos,canvas):
+    r = canvas.get_width()/20
+    pygame.draw.circle(canvas,white,pos,r,1)
+
+def map_(x,y,canvas):
+    '''change coordinates from 1-8 numbers to pixel coords'''
+    x_size,y_size = canvas.get_size()
+    a = (x_size/9 )*x+ x_size/18
+    b = (y_size/9 )*y+ y_size/18
+    return a,b
+
+def play(canvas):
+
+    a,b = map_(8,8,canvas)
+    draw_X(pos=(a,b),canvas=canvas)
+    # draw_O(pos=(a,b),canvas=canvas)
+
+
+
+    return True
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import numpy as np
+import game
 
 white   = 255,255,255
 red     = 255,  0,  0
@@ -44,40 +45,7 @@ def draw_board(size,canvas):
         d = size[1] , i*size[1]/3
         pygame.draw.line(canvas,red,a,b)
         pygame.draw.line(canvas,red,c,d)
-    
-def draw_X(pos,canvas):
-    size = canvas.get_size()
-    k = size[0]/20
-    l = size[1]/20
-    i = pos[0]-k,pos[1]-k
-    f = pos[0]+l,pos[1]+l
-    
-    i1 = pos[0]-k,pos[1]+k
-    f1 = pos[0]+k,pos[1]-k
 
-    pygame.draw.line(canvas,white,i,f)
-    pygame.draw.line(canvas,white,i1,f1)
-
-def draw_O(pos,canvas):
-    r = canvas.get_width()/20
-    pygame.draw.circle(canvas,white,pos,r,1)
-
-def map_(x,y,canvas):
-    '''change coordinates from 1-8 numbers to pixel coords'''
-    x_size,y_size = canvas.get_size()
-    a = (x_size/9 )*x+ x_size/18
-    b = (y_size/9 )*y+ y_size/18
-    return a,b
-
-def game(canvas):
-
-    a,b = map_(8,8,canvas)
-    draw_X(pos=(a,b),canvas=canvas)
-    # draw_O(pos=(a,b),canvas=canvas)
-
-
-
-    return True
 
 
 def main():
@@ -100,7 +68,7 @@ def main():
         canvas.fill(black)
         draw_board(size,canvas)
         
-        run = game(canvas)
+        run = game.play(canvas)
 
         pygame.display.flip()
         
